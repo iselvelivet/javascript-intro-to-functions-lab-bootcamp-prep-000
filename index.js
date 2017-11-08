91c58dc46f9bfeae77c017171b53944ce448e096
Tim Sander <tomzander88@yahoo.com>
2017-11-08 18:14:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,9 +11,9 @@ function logWhisper (string) {
   console.log('hello')
 }
 function sayHiToGrandma (string) {
-  if (string.toUpperCase === true) {
+  if (string === 'HELLO') {
     return 'YES INDEED'
-  } else if (string.toLowerCase === true) {
+  } else if (string === 'hello') {
     return 'I can\'t hear you!'
   } else if (string === 'I love you, Grandma.') {
     return 'I love you, too.'
